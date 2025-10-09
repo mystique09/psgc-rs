2715d78c0729f7d54135a0f3600328fd7b40c7be
mystique09 <knightkrypto7@gmail.com>
2025-10-09 23:53:45 +0800

    refactor: uuid_now

--- crates/psgc-infrastructure/src/database/generators.rs
+++ crates/psgc-infrastructure/src/database/generators.rs
@@ -2,7 +2,7 @@ use rbatis::rbdc::Uuid;
 
 pub fn uuid_now() -> Uuid {
     let uuid_v7 = uuid::Uuid::now_v7();
-    let id = rbatis::rbdc::Uuid(uuid_v7.to_string());
+    let id = Uuid(uuid_v7.to_string());
 
     id
 }
